3899ec425afbba7992b60e081523e33f4c44e1a0
Eva Decker <itsevadecker@gmail.com>
2024-05-21 16:24:47 -0400

    Install Playwright, ESLint, basic workplace settings for VS Code

---
+++ tests/index.spec.ts
@@ -0,0 +1,25 @@
+import AxeBuilder from '@axe-core/playwright';
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc');
+});
+
+test.describe('homepage', () => {
+  test('has title', async ({ page }) => {
+    await page.goto('/');
+  
+    await expect(page).toHaveTitle(/Namesake/);
+  });
+
+  test('should not have any automatically detectable accessibility issues', async ({ page }, testInfo) => {
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json'
+    });
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+});
+})
