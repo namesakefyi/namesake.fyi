5b8069dcabeeea068f514d7fa807c608bdebd0f9
Eva Decker <itsevadecker@gmail.com>
2024-05-27 12:31:58 -0400

    feat: Write longer content, split Header and Footer components (#23)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://namesake.fyi",
   integrations: [sitemap(), react()],
   devToolbar: {
-    enabled: false
-  }
+    enabled: false,
+  },
 });
