7a680cca76fe4c36fffe1f49d22180b53bfe373a
Eva Decker <itsevadecker@gmail.com>
2024-05-26 23:22:51 -0400

    feat: Add content and base styling (#15)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://namesake.fyi",
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
+  devToolbar: {
+    enabled: false
+  }
 });
