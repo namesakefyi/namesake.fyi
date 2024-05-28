c27afc85f1d0ccb29d170f2bed8e6f186760197a
Eva Decker <itsevadecker@gmail.com>
2024-05-28 19:29:27 -0400

    chore: Setup Astro content collections and Keystatic (#34)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
 
+// https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: cloudflare(),
   site: "https://namesake.fyi",
-  integrations: [sitemap(), react()],
+  integrations: [sitemap(), react(), markdoc(), keystatic()],
   vite: {
     ssr: {
       external: ["buffer", "path", "fs", "os", "crypto", "async_hooks"].map(
