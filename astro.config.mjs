5a39f0285f6fbb34d72f9a9505a6ce9037585e3b
Eva Decker <itsevadecker@gmail.com>
2024-05-28 02:23:18 -0400

    Use Alte Haas Grotesk everywhere

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  output: "hybrid",
+  adapter: cloudflare(),
   site: "https://namesake.fyi",
   integrations: [sitemap(), react()],
+  vite: {
+    ssr: {
+      external: ["buffer", "path", "fs", "os", "crypto", "async_hooks"].map(
+        (i) => `node:${i}`
+      ),
+    },
+  },
   devToolbar: {
     enabled: false,
   },
