efd5335884ce557555a63c819083bafa7cb8891f
Eva Decker <eva@namesake.fyi>
2024-09-03 00:03:57 -0400

    fix: Android mobile issues (#141)

--- src/data/colors.ts
+++ src/data/colors.ts
@@ -1,5 +1,6 @@
 type Color = {
   name: string;
+  slug: string;
   hex: string;
   forText?: boolean;
 };
@@ -17,36 +18,44 @@ export type NamesakeColor =
 export const colors: Record<NamesakeColor, Color> = {
   purple: {
     name: "Namesake Purple",
+    slug: "purple",
     hex: "#CDA5EF",
   },
   pink: {
     name: "Pastel Pink",
+    slug: "pink",
     hex: "#ECADD4",
   },
   blue: {
     name: "Pastel Blue",
+    slug: "blue",
     hex: "#96C7F2",
   },
   yellow: {
     name: "Pastel Yellow",
+    slug: "yellow",
     hex: "#ECDD85",
   },
   green: {
     name: "Pastel Green",
+    slug: "green",
     hex: "#97CF9C",
   },
   brown: {
     name: "Pastel Brown",
+    slug: "brown",
     hex: "#DDB896",
   },
   black: {
     name: "Photocopy Black",
+    slug: "black",
     hex: "#111111",
     forText: true,
   },
   white: {
     name: "Photocopy White",
-    hex: "#F1F1F1",
+    slug: "white",
+    hex: "#E1E1E1",
     forText: true,
   },
 };
