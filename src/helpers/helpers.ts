761052560ff1b465577a1d840b45e56b2b249ff9
Eva Decker <eva@namesake.fyi>
2024-12-13 12:21:25 -0500

    fix: Update annotation config spacing and iterations (#173)

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,4 +1,8 @@
 import { marked } from "marked";
+import type {
+  RoughAnnotationConfig,
+  RoughAnnotationType,
+} from "rough-notation/lib/model";
 
 export const smartquotes = (str: string) => {
   return str
@@ -18,4 +22,46 @@ export const fetchPolicy = async (slug: string) => {
   const markdownWithoutH1 = markdown.replace(/^# .*\n/gm, "");
   const content = marked.parse(markdownWithoutH1);
   return content;
+};
+
+export const annotationConfig: Record<
+  RoughAnnotationType,
+  Omit<RoughAnnotationConfig, "type">
+> = {
+  highlight: {
+    multiline: true,
+    iterations: 1,
+    padding: 0,
+  },
+  underline: {
+    multiline: true,
+    iterations: 3,
+    padding: 0,
+  },
+  box: {
+    multiline: false,
+    iterations: 3,
+    padding: [0, 8, 0, 8],
+  },
+  bracket: {
+    multiline: false,
+    iterations: 1,
+    padding: [0, 4, 0, 4],
+    brackets: ["left", "right"],
+  },
+  circle: {
+    multiline: false,
+    iterations: 2,
+    padding: [0, 16, 0, 16],
+  },
+  "strike-through": {
+    multiline: true,
+    iterations: 3,
+    padding: 0,
+  },
+  "crossed-off": {
+    multiline: false,
+    iterations: 4,
+    padding: 0,
+  },
 };
