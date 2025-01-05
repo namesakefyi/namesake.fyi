77f2ed19fc60dd2393661209320717d8a07a4aba
ginger <26461046+gingerchew@users.noreply.github.com>
2025-01-05 15:21:59 -0500

    Adds policy loader
    this pulls from the namesake.fyi policy repo and renders the content to html
    storing it and the headings metadata in the content layer.
    
    This makes it available like so:
    
    ```astro
    ---
    import { getEntry } from "astro:content";
    const entry = await getEntry('policy', 'terms');
    const  { Content, headings } = await render(entry);
    ---
    <Content />
    ```

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,85 @@
 import { glob } from "astro/loaders";
 import { z, defineCollection, reference } from "astro:content";
+import type { Loader, LoaderContext } from "astro/loaders";
 import type { RoughAnnotationType } from "rough-notation/lib/model";
 import type { NamesakeColor } from "~/data/colors";
+import { marked } from "marked";
+import type { MarkdownHeading } from "astro";
+
+type GithubTreeLeaf = {
+  path: string;
+  mode: string;
+  type: "tree" | "blob"; // tree is a directory, blob is a file
+  sha: string;
+  url: string;
+};
+
+type GithubTreeData = {
+  url: string;
+  hash: string;
+  tree: GithubTreeLeaf[];
+};
+
+function policyLoader(): Loader {
+  const gitTreeUrl =
+    "https://api.github.com/repos/namesakefyi/policies/git/trees/main?recursive=1";
+  const url = "https://raw.githubusercontent.com/namesakefyi/policies/main/";
+
+  const get = async <T>(url: string, type: "json" | "text"): Promise<T> => {
+    const result = await fetch(url);
+    const json = await result[type]();
+    return json;
+  };
+
+  const generateHeadingsFromText = (text: string): MarkdownHeading[] => {
+    // see...[https://regex101.com/r/n6XQub/4] & https://stackoverflow.com/questions/70801756/regex-extract-all-headers-from-markdown-string
+    const regXHeader = /(?<flag>#{1,6})\s+(?<content>.+)/g;
+
+    const headings = Array.from(
+      text.matchAll(regXHeader),
+      ({ groups }) =>
+        groups !== undefined && {
+          depth: groups.flag.length,
+          slug: groups.content
+            .toLowerCase()
+            .replaceAll(/\s/g, "_")
+            .replaceAll(/\,\.\(\)\[\]\+\?\!\'\"/g, ""),
+          text: groups.content,
+        },
+    ).filter(Boolean) as MarkdownHeading[];
+
+    return headings;
+  };
+
+  return {
+    name: "policy-loader",
+    load: async (context: LoaderContext) => {
+      const { tree } = await get<GithubTreeData>(gitTreeUrl, "json");
+
+      for await (const leaf of tree) {
+        // Can't do anything with a directory
+        if (leaf.type === "tree") continue;
+        const text = await get<string>(url + leaf.path, "text");
+        const digest = context.generateDigest(text);
+
+        const [id, extension] = leaf.path.split(".");
+        context.store.set({
+          id,
+          // Need to pass an empty object to appease the typescript gods
+          data: {},
+          body: text,
+          rendered: {
+            html: await marked.parse(text.replace(/^# .*\n/gm, "")),
+            metadata: {
+              headings: generateHeadingsFromText(text),
+            },
+          },
+          digest,
+        });
+      }
+    },
+  };
+}
 
 export const collections = {
   authors: defineCollection({
@@ -85,5 +163,8 @@ export const collections = {
           })
           .optional(),
       }),
+  }),
+  policy: defineCollection({
+    loader: policyLoader(),
   }),
 };
