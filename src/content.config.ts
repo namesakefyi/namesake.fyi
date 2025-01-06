9911a4e9daf003b063176bf50b96cc17f7e27319
ginger <26461046+gingerchew@users.noreply.github.com>
2025-01-06 10:12:22 -0500

    Converts the policy loader to use the internal @astrojs/markdown-remark pkg
    Thankfully the way the markdown package works, we're able to get it with very little need
    for documenting on our own as it is very straight forward.
    
    1. Get config from LoaderContext
    2. Create markdownProcessor
    3. Use processor
    4. Change the names of the output to match what the data store expects
    5. Profit

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,10 @@
 import { glob } from "astro/loaders";
 import { z, defineCollection, reference } from "astro:content";
+import type { RenderedContent } from "astro:content";
 import type { Loader, LoaderContext } from "astro/loaders";
 import type { RoughAnnotationType } from "rough-notation/lib/model";
 import type { NamesakeColor } from "~/data/colors";
-import { marked } from "marked";
-import type { MarkdownHeading } from "astro";
+import { createMarkdownProcessor } from "@astrojs/markdown-remark";
 
 type GithubTreeLeaf = {
   path: string;
@@ -27,38 +27,23 @@ function policyLoader(): Loader {
 
   const get = async <T>(url: string, type: "json" | "text"): Promise<T> => {
     const result = await fetch(url);
-    const json = await result[type]();
-    return json;
-  };
-
-  const generateHeadingsFromText = (text: string): MarkdownHeading[] => {
-    // see...[https://regex101.com/r/n6XQub/4] & https://stackoverflow.com/questions/70801756/regex-extract-all-headers-from-markdown-string
-    const regXHeader = /(?<flag>#{1,6})\s+(?<content>.+)/g;
-
-    const headings = Array.from(
-      text.matchAll(regXHeader),
-      ({ groups }) =>
-        groups !== undefined && {
-          depth: groups.flag.length,
-          slug: groups.content
-            .toLowerCase()
-            .replaceAll(/\s/g, "-")
-            .replaceAll(/\,\.\(\)\[\]\+\?\!\'\"/g, ""),
-          text: groups.content,
-        },
-    ).filter(Boolean) as MarkdownHeading[];
-
-    return headings;
+    const final = await result[type]();
+    return final;
   };
 
   return {
     name: "policy-loader",
     load: async (context: LoaderContext) => {
+      const processor = await createMarkdownProcessor(context.config.markdown);
       const { tree } = await get<GithubTreeData>(gitTreeUrl, "json");
+
       for await (const leaf of tree) {
         // Can't do anything with a directory
         if (leaf.type === "tree") continue;
         const text = await get<string>(url + leaf.path, "text");
+
+        const { code: html, metadata } = await processor.render(text);
+
         const digest = context.generateDigest(text);
 
         const [id] = leaf.path.split(".");
@@ -68,10 +53,8 @@ function policyLoader(): Loader {
           data: {},
           body: text,
           rendered: {
-            html: await marked.parse(text.replace(/^# .*\n/gm, "")),
-            metadata: {
-              headings: generateHeadingsFromText(text),
-            },
+            html,
+            metadata,
           },
           digest,
         });
