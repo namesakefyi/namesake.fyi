c9c909fdabca9575108223caeb28e3ceb2570fb8
Eva Decker <eva@namesake.fyi>
2025-06-17 20:10:39 -0400

    feat: Re-enable Keystatic (#219)

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -119,24 +119,18 @@ export default config({
             itemLabel: (props) => props.value ?? "Unknown",
           },
         ),
-        image: fields.conditional(
-          fields.checkbox({ label: "Include image?", defaultValue: false }),
-          {
-            true: fields.object({
-              src: fields.image({
-                label: "Image",
-                description:
-                  "Images will automatically have filters applied to match the background color of the page.",
-                directory: "src/assets/images/posts",
-                publicPath: "../../assets/images/posts/",
-              }),
-              alt: fields.text({
-                label: "Alt Text",
-              }),
-            }),
-            false: fields.empty(),
-          },
-        ),
+        image: fields.object({
+          src: fields.image({
+            label: "Image",
+            description:
+              "Images will automatically have filters applied to match the background color of the page.",
+            directory: "src/assets/images/posts",
+            publicPath: "../../assets/images/posts/",
+          }),
+          alt: fields.text({
+            label: "Alt Text",
+          }),
+        }),
         content: fields.markdoc({
           label: "Content",
           extension: "md",
@@ -174,22 +168,16 @@ export default config({
             "The name of the publication (use full spelling, no acronyms)",
           validation: { isRequired: true },
         }),
-        image: fields.conditional(
-          fields.checkbox({ label: "Include image?", defaultValue: false }),
-          {
-            true: fields.object({
-              src: fields.image({
-                label: "Image",
-                directory: "src/assets/images/press",
-                publicPath: "../../assets/images/press/",
-              }),
-              alt: fields.text({
-                label: "Alt Text",
-              }),
-            }),
-            false: fields.empty(),
-          },
-        ),
+        image: fields.object({
+          src: fields.image({
+            label: "Image",
+            directory: "src/assets/images/press",
+            publicPath: "../../assets/images/press/",
+          }),
+          alt: fields.text({
+            label: "Alt Text",
+          }),
+        }),
       },
     }),
   },
