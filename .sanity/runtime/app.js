b36c8d93538f1c88b5e2f6ebb306d06dfc454282
Eva Decker <itsevadecker@gmail.com>
2025-09-20 00:47:58 -0400

    Remove .sanity dir

--- .sanity/runtime/app.js
+++
@@ -1,9 +0,0 @@
-import { renderStudio } from "sanity";
-// This file is auto-generated on 'sanity dev'
-// Modifications to this file are automatically discarded
-import studioConfig from "../../sanity.config.ts";
-
-renderStudio(document.getElementById("sanity"), studioConfig, {
-  reactStrictMode: false,
-  basePath: "/",
-});
