7ba9b0bb9e0939782440a591d256d09c0f00ce06
Eva Decker <itsevadecker@gmail.com>
2025-11-26 11:06:43 -0500

    Replace react-player with lite-youtube for smaller bundle

--- src/sanity/components/YouTubePreview.tsx
+++ src/sanity/components/YouTubePreview.tsx
@@ -1,14 +1,17 @@
 import { Flex, Text } from "@sanity/ui";
-import ReactPlayer from "react-player";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 import type { PreviewProps } from "sanity";
 
 export function YouTubePreview(props: PreviewProps) {
   const { title: url } = props;
 
+  const videoId = typeof url === "string" ? url.split("v=")[1] : null;
+
   return (
     <Flex padding={3} align="center" justify="center">
-      {typeof url === "string" ? (
-        <ReactPlayer src={url} />
+      {videoId ? (
+        <LiteYouTubeEmbed title="" id={videoId} />
       ) : (
         <Text>Add a YouTube URL</Text>
       )}
