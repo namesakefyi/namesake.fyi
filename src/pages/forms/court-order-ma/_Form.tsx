79eadb851170cfef27a4bf8e8f41f870bc87b50d
Eva Decker <eva@namesake.fyi>
2025-12-04 01:02:48 -0500

    Remove waiver steps from MA Court Order (#299)

--- src/pages/forms/court-order-ma/_Form.tsx
+++ src/pages/forms/court-order-ma/_Form.tsx
@@ -1,3 +1,5 @@
+import { RiMegaphoneLine } from "@remixicon/react";
+import { Banner } from "@/components/react/common/Banner";
 import { FormContainer } from "@/components/react/forms/FormContainer";
 import type { FieldType } from "@/constants/fields";
 import { createFormSubmitHandler } from "@/utils/createFormSubmitHandler";
@@ -37,6 +39,20 @@ export function MaCourtOrderForm({
       updatedAt={updatedAt}
       pdfs={pdfs}
       costs={costs}
-    />
+    >
+      <Banner icon={RiMegaphoneLine}>
+        Massachusetts no longer requires publishing name change in a newspaper
+        as of November 26, 2025. All name change records will now be kept
+        confidential without having to file additional paperwork.{" "}
+        <a
+          href="https://www.masstpc.org/name-change-bill-signed/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read more about the passage of MA Senate Bill 1045 and House Bill
+          1673, "An Act Protecting Personal Security".
+        </a>
+      </Banner>
+    </FormContainer>
   );
 }
